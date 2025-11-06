c61657ab7185986bcab4242e425af0970ce9a4b5
Nikhil Komalla <146886084+Nikhil-Komalla@users.noreply.github.com>
2025-11-06 10:52:52 -0500

    Add files via upload
    # DOS_plotter.py
    Is a modified version of below PDOS code, with d_band center addition, slight changes in plot.
    [https://github.com/nishantaMishra/BatteryInformatics/tree/main/DOS-plots/PDOS](url)
    # wulff_construction.py
    Utilizes wulff pack [https://wulffpack.materialsmodeling.org/](url)

---
+++ wulff_construction.py
@@ -0,0 +1,27 @@
+from wulffpack import (SingleCrystal,
+Decahedron,
+Icosahedron)
+from ase.build import bulk
+from ase.io import read, write
+import matplotlib.pyplot as plt
+
+
+prim = read('POSCAR', format='vasp')
+
+surface_energies = {(0, 0, 1): 0.84,
+(0, 1,1): 1.51,
+(1, 1, 1): 1.19}
+
+
+
+particle = SingleCrystal(surface_energies,
+primitive_structure=prim,
+natoms=5000)
+
+#colors = {(1, 0, 0): '#0053b5',
+#(1, 1, 0): "#ddd726",
+#(1, 1, 1): "#9538db"}
+
+# This will now create an interactive plot
+particle.view()
+#particle.view(colors=colors)
